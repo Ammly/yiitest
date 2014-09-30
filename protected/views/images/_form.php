7f200f870c690381f2964053ff587d0be41af6ee
Ammly <ammlyf@gmail.com>
2014-09-30 16:35:26 +0300

    Trying Yii Framework
    Make changes to Protected\ Views or Models

---
+++ protected/views/images/_form.php
@@ -0,0 +1,36 @@
+<?php
+/* @var $this ImagesController */
+/* @var $model Images */
+/* @var $form CActiveForm */
+?>
+
+<div class="form">
+
+<?php $form=$this->beginWidget('CActiveForm', array(
+	'id'=>'images-form',
+	'enableAjaxValidation'=>false,
+)); ?>
+
+	<p class="note">Fields with <span class="required">*</span> are required.</p>
+
+	<?php echo $form->errorSummary($model); ?>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'name'); ?>
+		<?php echo $form->textField($model,'name',array('size'=>25,'maxlength'=>25)); ?>
+		<?php echo $form->error($model,'name'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'path'); ?>
+		<?php echo $form->textField($model,'path',array('size'=>60,'maxlength'=>255)); ?>
+		<?php echo $form->error($model,'path'); ?>
+	</div>
+
+	<div class="row buttons">
+		<?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); ?>
+	</div>
+
+<?php $this->endWidget(); ?>
+
+</div><!-- form -->
